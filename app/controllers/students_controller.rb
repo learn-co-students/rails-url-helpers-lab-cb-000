ef68a7825e92d9e66eddc0831671ef009032dff7
Eleanor Thomas <eleanor.thomas.res@gmail.com>
2017-06-30 00:24:04 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,18 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: :show
-  
+  protect_from_forgery with: :null_session
+  before_action :set_student, only: [:show, :update]
+
   def index
     @students = Student.all
   end
 
   def show
+  end
+
+  def update
+    @student.active = !@student.active
+    @student.save
+    redirect_to student_path(@student)
   end
 
   private
