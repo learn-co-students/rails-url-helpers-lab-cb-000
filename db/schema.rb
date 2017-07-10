5f078d3ca91a23aa273941f87c5e9084662b0ac6
Mariel Segovia <msegovia94@gmail.com>
2017-07-10 22:41:40 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 20170709080337) do
     t.string   "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean  "active"
+    t.boolean  "active", default: false
   end
 
 end
