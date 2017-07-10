5a67903370649b109022cd232dc5e14c42404221
Mariel Segovia <msegovia94@gmail.com>
2017-07-10 22:35:53 +0000

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,14 @@
 class Student < ActiveRecord::Base
+
+  after_initialize :default_value
+
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  private
+  def default_value
+    self.active ||= false
+  end
+
 end
