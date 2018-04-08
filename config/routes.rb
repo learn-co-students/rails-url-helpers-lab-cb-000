dca7adee695765958d40d9c399994a3120aa734b
adam n <adam.nawroc@gmail.com>
2018-04-08 13:29:34 +0000

    progress

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   # resources :students, only: [:index, :show]
   # However, for the sake of this lab and seeing how you can pass params through
   # the route, we'll keep it explicit for now and refactor later.
+
+  # think they wanted u to use :as
   get "students/:id", to: "students#show", as: :student
   # pulled out of nowhere.
   get "students/:id/activate" => "students#activate", as: :activate_student
